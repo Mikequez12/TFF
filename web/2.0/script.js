4dd61f66afcab8e684be7d39dd6a0c38e0e0aced
Mike <142720633+Mikequez12@users.noreply.github.com>
2024-08-18 22:54:48 +0200

    update

---
+++ web/2.0/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('game').forEach((game) => {
+        game.parentElement.innerHTML = `
+        <div>
+            <h3>${game.getAttribute("name")}</h3>
+            <span${game.innerText}></span>
+        </div>
+        <img src="assets/games/${game.getAttribute("alias")}/logo.png"></img>
+        `
+    })
+});
